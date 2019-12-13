d87563528aef4ce5473e6fd6f4ee0820f977ef4d
Randall Williams <randallw1996@gmail.com>
2019-12-13 18:48:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
         }
      }
      
-  programmer_hash[:alan_kay]["#{:known_for}"]
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie]["#{:languages}"]
 end
 
 def adding_matz
